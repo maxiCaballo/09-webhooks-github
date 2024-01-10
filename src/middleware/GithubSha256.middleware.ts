3e8ff26ec21f54d9893bcadf3e94edce9932c20d
maxiCaballo <caballomaxi@gmail.com>
2024-01-10 15:57:47 -0300

    Proyecto actualizado con verificacion token de github

---
+++ src/middleware/GithubSha256.middleware.ts
@@ -0,0 +1,30 @@
+import * as crypto from 'crypto';
+
+import { Request, Response, NextFunction } from 'express';
+import { envs } from '../config';
+
+const SECRET_TOKEN: string = envs.SECRET_TOKEN;
+
+const verify_signature = (req: Request) => {
+	try {
+		const signature = crypto.createHmac('sha256', SECRET_TOKEN).update(JSON.stringify(req.body)).digest('hex');
+		const xHubSignature = req.header('x-hub-signature-256') ?? '';
+
+		let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+		let untrusted = Buffer.from(xHubSignature, 'ascii');
+		return crypto.timingSafeEqual(trusted, untrusted);
+	} catch (error) {
+		return false;
+	}
+};
+
+export class GithubSha256Middleware {
+	static verifySignature = (req: Request, res: Response, next: NextFunction) => {
+		if (!verify_signature(req)) {
+			res.status(401).send('Unauthorized');
+			return;
+		}
+
+		next();
+	};
+}
